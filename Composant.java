062caa1b6022997dc90a4baab3c2c239e896f95d
Leandro Schoonewolff <wbbeyblade@gmail.com>
2024-02-15 17:00:54 -0500

    Debut des deux classes(sont encore en brouillon)

--- Composant.java
+++ Composant.java
@@ -1,3 +1,84 @@
+import java.util.Locale;
+
 public class Composant {
-    
+    private final String categorie;
+    private String marque;
+    private String nom;
+    private double prix;
+    private double rabais;
+
+    public Composant(String categorie,String marque, String nom, double prix){
+        rabais = 0;
+        this.categorie = categorie.toUpperCase();
+        this.marque = marque;
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public Composant copier(){
+        return new Composant(this.categorie,this.marque,this.nom,this.prix);
+    }
+
+    public boolean estIdentique(Composant autreComposant){
+        return this.getCategorie().equals(autreComposant.getCategorie()) && this.getMarque().equals(autreComposant.getMarque()) && this.getNom().equals(autreComposant.getNom());
+
+    }
+
+    public String toString(){
+        return "[" + getCategorie() + "] " + getCategorie() + " " + getNom();
+    }
+
+
+//Accesseurs et mutateurs
+    public double getPrix() {
+        return prix - (prix * (getRabais() / 100));
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getRabais() {
+        return rabais;
+    }
+/*
+    public void setCategorie(String categorie) {
+        this.categorie = categorie.toUpperCase();
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+*/
+    public void setPrix(double prix) {
+        if(prix > 0){
+            this.prix = prix;
+        }
+        else{
+            System.out.println("Erreur le prix entré est négatif");
+        }
+
+    }
+
+    public void setRabais(double rabais) {
+        if(rabais > 0 && rabais <= 100){
+            this.rabais = rabais;
+        }
+        else{
+            System.out.println("Erreur le rabais dot être entre 0 et 100%(inclus)");
+        }
+
+    }
 }
--- Configuration.java
+++ Configuration.java
@@ -1,3 +1,32 @@
 public class Configuration {
+    private static String description;
+    private int prix_max;
+    private Composant[] composantes;
+    private int nb_compsants;
+    private final int MAX_COMPOSANTS = 20;
+
     
+
+
+    //Accesseurs et Mutateurs
+
+    public static String getDescription() {
+        return description;
+    }
+
+    public int getPrix_max() {
+        return prix_max;
+    }
+
+    public Composant[] getComposantes() {
+        return composantes;
+    }
+
+    public int getNb_compsants() {
+        return nb_compsants;
+    }
+
+
+
+
 }
