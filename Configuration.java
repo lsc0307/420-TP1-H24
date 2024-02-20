57d62c9ef79dca4c78ad6140c75a218a2aab3130
Leandro Schoonewolff <wbbeyblade@gmail.com>
2024-02-20 07:27:25 -0500

    deuxieme journée de travail et avance de la classe Configuration

--- Configuration.java
+++ Configuration.java
@@ -1,11 +1,56 @@
 public class Configuration {
     private static String description;
-    private int prix_max;
-    private Composant[] composantes;
-    private int nb_compsants;
+    private double prix_max;
+    private Composant[] composants;
+    private int nb_composants;
     private final int MAX_COMPOSANTS = 20;
 
-    
+    public Configuration(String description,double prix_max,Composant[] composants){
+        this.description = description;
+        this.prix_max = prix_max;
+        this.composants = composants;
+    }
+
+    public Configuration(Configuration config){
+        this.description = config.description;
+        this.prix_max = config.prix_max;
+        this.composants = new Composant[config.composants.length];
+        for (int i = 0; i < config.composants.length; i++) {
+            this.composants[i] = config.composants[i];
+        }
+    }
+
+    public double calculerTotal(double taxe){
+        double prixTotal=0;
+        for(int i=0;i< composants.length;i++){
+            prixTotal += composants[i].getPrix();
+        }
+        return prixTotal;
+    }
+
+    public Composant rechercher(String categorie){
+        for(int i=0;i< composants.length;i++){
+            if(composants[i].getCategorie().equals(categorie)){
+                return composants[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean remplacer(Composant composant){
+        if(rechercher(composant.getCategorie()) == null){
+            return false;
+        }
+        for(int i=0;i<composants.length;i++){
+            if(composants[i].getCategorie().equals(composant.getCategorie()) && (prix_max-composants[i].getPrix()) + composant.getPrix() <= prix_max){
+                composants[i] = composant;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 
     //Accesseurs et Mutateurs
@@ -14,16 +59,16 @@ public class Configuration {
         return description;
     }
 
-    public int getPrix_max() {
+    public double getPrix_max() {
         return prix_max;
     }
 
-    public Composant[] getComposantes() {
-        return composantes;
+    public Composant[] getComposants() {
+        return composants;
     }
 
-    public int getNb_compsants() {
-        return nb_compsants;
+    public int getNb_composants() {
+        return nb_composants;
     }
 
 
